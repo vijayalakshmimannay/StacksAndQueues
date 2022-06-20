ea8ae54bbd76fd5178504e00790bff78545da882
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 22:23:01 +0530

    [vijayalakshmi] adding element into Queue

---
+++ StacksAndQueues/LinkedListQueues.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace StacksAndQueues
+{
+    public class LinkedListQueues
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+
+        internal void DisPlay()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("{0} is in the top of the Queue", this.head.data);
+        }
+
+    }
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,13 +7,21 @@ namespace StacksAndQueues
         {
             Console.WriteLine("Hello, World!");
             LinkedListStack value = new LinkedListStack();
-            value.push(70);
-            value.push(30);
-            value.push(56);
-            value.DisPlay();
-            value.Peek();
-            value.Pop();
-            value.IsEmpty();
+            //value.push(70);
+            // value.push(30);
+            // value.push(56);
+            //value.DisPlay();
+            //value.Peek();
+            //value.Pop();
+            // value.IsEmpty();
+
+
+
+            LinkedListQueues example = new LinkedListQueues();
+            example.Enqueue(56);
+            example.Enqueue(30);
+            example.Enqueue(70);
+            example.DisPlay();
 
         }
     }
